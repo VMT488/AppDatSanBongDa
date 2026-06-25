2df5f0021e4aeedb7bbf04f932fcf580f2a309f8
TienTran <23130331@st.hcmuaf.edu.vn>
2026-06-25 19:36:01 +0700

    them import firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,7 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation(platform("com.google.firebase:firebase-bom:34.15.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
 }
